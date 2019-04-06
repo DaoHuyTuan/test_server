ca5a7e08c29ea3427787f725daf8df217fee8ec2
daohuytuan <tuandh95@gmail.com>
2019-04-06 16:21:32 +0700

    config cors

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const db = require("./queries")
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-
+app.use(cors());
+app.options('*', cors());
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
         extended:true
     })
 )
-app.get("/",db.getData) 
+app.get("*/",db.getData) 
 app.listen(port, () => {
     console.log("server is runing on port " + port)
 })
